e49e92e401c9af3e2bb65cba3434806fff8ae8af
Iceline King <icedking23@outlook.com>
2024-04-30 13:45:52 -0400

    first activity in class

---
+++ Functions/simple_functions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function favoriteThings() {
+  console.log("Kiwis");
+  console.log("Papayas");
+  console.log("Cranberries");
+}
+
+function whyImHere() {
+  console.log("I'm here to become a master programmer and coder");
+}
+
+function favoritePlace() {
+  console.log("My favorite place is Pandora because it's beautiful");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
+
+
